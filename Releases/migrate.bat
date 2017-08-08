148ad5fc416c39879ff8f5fb3c9b9bbd726e1a06
mohan252 <mohan252@gmail.com>
2017-08-08 19:26:18 +0530

    changed the location of fluent migrator

--- Releases/migrate.bat
+++ Releases/migrate.bat
@@ -1,5 +1,6 @@
 if exist "C:\Work\billing_app\packages\FluentMigrator.1.6.2\tools\Migrate.exe" (
     C:\Work\billing_app\packages\FluentMigrator.1.6.2\tools\Migrate.exe -a %~dp0\DbMigrations.dll -db SqlServer2008 -conn "Data Source=.\SQLEXPRESS;Initial Catalog=Company;Integrated Security=False;Persist Security Info=True;User ID=sa;Password=password123;Connect Timeout=30"
 ) else (
-    D:\Work\billing_app\packages\FluentMigrator.1.6.2\tools\Migrate.exe -a %~dp0\DbMigrations.dll -db SqlServer2008 -conn "Data Source=.\SQLEXPRESS;Initial Catalog=Company;Integrated Security=False;Persist Security Info=True;User ID=sa;Password=password123;Connect Timeout=30"
+    D:\billing_app\packages\FluentMigrator.1.6.2\tools\Migrate.exe -a %~dp0\DbMigrations.dll -db SqlServer2008 -conn "Data Source=.\SQLEXPRESS;Initial Catalog=Company;Integrated Security=False;Persist Security Info=True;User ID=sa;Password=password123;Connect Timeout=30"
 )
+pause
