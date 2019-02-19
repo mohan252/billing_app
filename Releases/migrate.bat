e577e7ee1126a869371c60c9fa49a480ad64f177
Mohan Thirunavukkarasu <mthirunavukkarasu@carbonite.com>
2019-02-19 12:19:04 -0500

    removed releases folder

--- Releases/migrate.bat
+++
@@ -1,6 +0,0 @@
-if exist "C:\Work\billing_app\packages\FluentMigrator.1.6.2\tools\Migrate.exe" (
-    C:\Work\billing_app\packages\FluentMigrator.1.6.2\tools\Migrate.exe -a %~dp0\DbMigrations.dll -db SqlServer2008 -conn "Data Source=.\SQLEXPRESS;Initial Catalog=Company;Integrated Security=False;Persist Security Info=True;User ID=sa;Password=password123;Connect Timeout=30"
-) else (
-    D:\billing_app\packages\FluentMigrator.1.6.2\tools\Migrate.exe -a %~dp0\DbMigrations.dll -db SqlServer2008 -conn "Data Source=.\SQLEXPRESS;Initial Catalog=Company;Integrated Security=False;Persist Security Info=True;User ID=sa;Password=password123;Connect Timeout=30"
-)
-pause
