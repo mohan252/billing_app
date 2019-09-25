75e8522e7188d8b953d9502e525d0c45873edd7b
Mohan Thirunavukkarasu <mthirunavukkarasu@carbonite.com>
2019-09-25 07:25:29 -0400

    added debit note and debit note items table

---
+++ DbMigrations/M011_AddDebitNoteTable.cs
@@ -0,0 +1,23 @@
+﻿using FluentMigrator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbMigrations
+{
+    [Migration(11)]
+    public class M011_AddDebitNoteAndItemsTable : Migration
+    {
+        public override void Down()
+        {            
+        }
+
+        public override void Up()
+        {
+            Execute.EmbeddedScript("M011_CreateDebitNoteTable.sql");
+            Execute.EmbeddedScript("M011_CreateDebitNoteItemsTable.sql");
+        }
+    }
+}
